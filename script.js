58e593da33a5f25c0dac80cff76f4c86126c8e87
Evgeniy Karsiuk <karsiuk0@gmail.com>
2022-07-26 22:22:05 +0300

    day8.1

--- script.js
+++ script.js
@@ -99,4 +99,22 @@ function playerRankUp (points) {
     mutation(["hello", "Hello"]);
     mutation(["Mary", "Army"]);
     mutation(["Mary", "Aarmy"]);
-    mutation(["Noel", "Ole"]); 
+    mutation(["Noel", "Ole"]);
+
+          class Book {
+  constructor(author) {
+    this._author = author;
+  }
+  // getter
+  get writer() {
+    return this._author;
+  }
+  // setter
+  set writer(updatedAuthor) {
+    this._author = updatedAuthor;
+  }
+}
+const novel = new Book('anonymous');
+console.log(novel.writer);
+novel.writer = 'newAuthor';
+console.log(novel.writer);
