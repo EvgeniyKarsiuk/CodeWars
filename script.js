fe26a5359eb9755c655cf77491ca86edf54ba64a
Evgeniy Karsiuk <karsiuk0@gmail.com>
2022-08-09 22:27:48 +0300

    fry

--- script.js
+++ script.js
@@ -44,7 +44,3 @@ function mergeArrays(arr1, arr2) {
 }
 
 
-let hello = "   Hello, World!  ";
-let wsRegex = /^\s+|\s+$/g; // Change this line
-let result = hello.replace(wsRegex, ""); // Change this line
-
